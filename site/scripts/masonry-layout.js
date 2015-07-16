f72af88745141d54a42717a88131ed03db8a7499
Chris Ryan <creditdesign@gmail.com>
2015-07-16 14:25:43 +0100

    Adds audio video page

---
+++ site/scripts/masonry-layout.js
@@ -0,0 +1,37 @@
+// Audio video layout
+
+(function() {
+
+	var init = function($) {
+
+			var grid = $('.grid').masonry({
+				// options...
+				columnWidth: ".grid-item",
+				itemSelector: ".grid-item",
+				gutter: ".grid-gutter",
+				percentPosition: true
+			});
+
+			// layout Masonry after each image loads
+			grid.imagesLoaded().progress( function() {
+				grid.masonry();
+			});
+
+			var btn = $(".media-dismiss-btn");
+
+			btn.click(function() {
+				var parent = $(this).parents(".grid-item")[0];
+
+				grid.masonry( 'remove', parent ).masonry();
+			});
+
+	};
+
+	setTimeout(function() {
+		if (typeof jQuery !== 'undefined') {
+			init(jQuery);
+		} else {
+			setTimeout(arguments.callee, 60);
+		}
+	}, 60);
+})();
